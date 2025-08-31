20abb3a623456debfeacc86b1fcae44ca3df4987
yashG <yrg.creation.ai@gmail.com>
2025-08-31 21:59:55 +0530

    route path added in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Agence from './pages/Agence'
+import Projects from './pages/Projects'
 
 const App = () => {
   return (
-    <div>App
-      <h3>hello from yash..</h3>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/agence' element={<Agence />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
     </div>
   )
 }

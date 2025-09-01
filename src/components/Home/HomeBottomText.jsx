3c23dbbcdfe5918660e84a4321d5e2c5099819f0
yashG <yrg.creation.ai@gmail.com>
2025-09-01 22:01:05 +0530

    added bototm links page

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -3,9 +3,9 @@ import { Link, Routes, Route } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div className='flex items-center justify-center gap-2 bg-red-600'>
-      <Link className= "text-[5.6vw] uppercase border-4 border-white rounded-full px-6 " >Projects</Link>
-      <Link className= "text-[5.6vw] uppercase border-4 border-white rounded-full px-6 " >Agence</Link>
+    <div className='flex items-center justify-center gap-2'>
+      <Link className= "text-[5.6vw] uppercase border-4 border-white rounded-full px-5 " >Projects</Link>
+      <Link className= "text-[5.6vw] uppercase border-4 border-white rounded-full px-5 " >Agence</Link>
     </div>
   )
 }

5eee1351e15bc326c87d20e1973f49b83e0d51e5
yashG <yrg.creation.ai@gmail.com>
2025-09-01 21:43:09 +0530

    working on home bottom text

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link, Routes, Route } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div>HomeBottomText</div>
+    <div className='flex items-center justify-center gap-2 bg-red-600'>
+      <Link className= "text-[5.6vw] uppercase border-4 border-white rounded-full px-6 " >Projects</Link>
+      <Link className= "text-[5.6vw] uppercase border-4 border-white rounded-full px-6 " >Agence</Link>
+    </div>
   )
 }
 
--- src/components/Home/HomeHeroText.jsx
+++ src/components/Home/HomeHeroText.jsx
@@ -4,19 +4,19 @@ import Video from './video'
 const HomeHeroText = () => {
   return (
     <div className="hero-t pt-2 text-center">
-        <div className='text-[8.5vw] uppercase leading-[8vw] flex items-center justify-center' >
+        <div className='text-[8.5vw] leading-[8vw] flex items-center justify-center' >
            The spark for
         </div>
-        <div className='text-[8.5vw] uppercase leading-[8vw] flex items-center justify-center' >
+        <div className='text-[8.5vw] leading-[8vw] flex items-center justify-center' >
            all 
            <div className='h-[7vw] w-[14vw] rounded-full overflow-hidden'> <Video /> </div> 
            things
         </div>
-        <div className='text-[8.5vw] uppercase leading-[8vw] flex items-center justify-center' >
+        <div className='text-[8.5vw] leading-[8vw] flex items-center justify-center' >
             creative
         </div>
     </div>
   )
 }
 
-export default HomeHeroText
+export default HomeHeroText 

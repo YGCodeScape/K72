9fc997587c4daba1b4bca0e1d5efc53a358f17ba
yashG <yrg.creation.ai@gmail.com>
2025-09-01 21:24:52 +0530

    home text video added

--- src/components/Home/HomeHeroText.jsx
+++ src/components/Home/HomeHeroText.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import Video from './video'
 
 const HomeHeroText = () => {
   return (
-    <div className="hero-t">
-      <div className='text-[6vw] uppercase leading-[5vw]' >The spark for</div>
-      <div className='text-[6vw] uppercase leading-[5vw]' >all things</div>
-      <div className='text-[6vw] uppercase leading-[5vw]' >creative</div>
+    <div className="hero-t pt-2 text-center">
+        <div className='text-[8.5vw] uppercase leading-[8vw] flex items-center justify-center' >
+           The spark for
+        </div>
+        <div className='text-[8.5vw] uppercase leading-[8vw] flex items-center justify-center' >
+           all 
+           <div className='h-[7vw] w-[14vw] rounded-full overflow-hidden'> <Video /> </div> 
+           things
+        </div>
+        <div className='text-[8.5vw] uppercase leading-[8vw] flex items-center justify-center' >
+            creative
+        </div>
     </div>
   )
 }

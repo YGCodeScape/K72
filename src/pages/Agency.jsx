d2e2ffacce6cb4478b8a40972349d6f6964bd229
yashG <yrg.creation.ai@gmail.com>
2025-09-09 21:20:44 +0530

    agency page 1 done

--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -2,23 +2,25 @@ import React from 'react'
 
 const Agency = () => {
   return (
-    <div className=''>
-       <div className=" mt-[55vh]">
-           <h1 className='text-[19.5vw] uppercase leading-[16.5vw] text-center'>Soixan7t <br/>Twelve
-           </h1>
-       </div>
-       <div className='mt-[2rem]'>
-        <p className='text-[3.1rem] font-[500] w-[60%] ml-[42%] leading-[10vh]'>
-          &emsp;&emsp;&emsp;&emsp;&emsp; Our curiosity fuels our creativity. 
-          We remain humble and say no to big egos, 
-          even yours. A brand is alive. She has values,
-           a personality, a history. If we forget that, we 
-           can make good numbers in the short term, but we 
-           kill it in the long term. This is why we are 
-           committed to giving perspective, to building 
-           influential brands.
-        </p>
-       </div>
+    <div className='Agency-container'>
+      <div className="ag-p1">
+         <div className=" mt-[50vh]">
+             <h1 className='text-[19.5vw] uppercase leading-[16.5vw] text-center font-[700]'>Soixan7t <br/>Twelve
+             </h1>
+         </div>
+         <div className='mt-[2rem]'>
+             <p className='text-[3.1rem] font-[500] w-[60%] ml-[40%] leading-[10vh]'>
+                 &emsp;&emsp;&emsp;&emsp;&emsp; Our curiosity fuels our creativity. 
+                 We remain humble and say no to big egos, 
+                 even yours. A brand is alive. She has values,
+                 a personality, a history. If we forget that, we 
+                 can make good numbers in the short term, but we 
+                 kill it in the long term. This is why we are 
+                 committed to giving perspective, to building 
+                 influential brands.
+             </p>
+         </div>
+      </div>
     </div>
   )
 }

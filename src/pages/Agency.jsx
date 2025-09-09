dbe2bc1d0039d2a9e985099de6bd27bde20eff51
yashG <yrg.creation.ai@gmail.com>
2025-09-09 21:45:48 +0530

    working on ag sec 2

--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -2,28 +2,34 @@ import React from 'react'
 
 const Agency = () => {
   return (
-    <div className='Agency-p1-container'>
-      <div className="ag-p1-img-div h-70 w-52 absolute top-[20%] left-[30%] rounded-[1rem] -z-2 bg-red-700 ">
-        {/* <img src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" /> */}
-      </div>
-      <div className="ag-p1-text">
-         <div className=" mt-[50vh]">
-             <h1 className='text-[19.5vw] uppercase leading-[16.5vw] text-center font-[700]'>Soixan7t <br/>Twelve
-             </h1>
-         </div>
-         <div className='mt-[2rem]'>
-             <p className='text-[3.1rem] font-[500] w-[60%] ml-[40%] leading-[10vh]'>
-                 &emsp;&emsp;&emsp;&emsp;&emsp; Our curiosity fuels our creativity. 
-                 We remain humble and say no to big egos, 
-                 even yours. A brand is alive. She has values,
-                 a personality, a history. If we forget that, we 
-                 can make good numbers in the short term, but we 
-                 kill it in the long term. This is why we are 
-                 committed to giving perspective, to building 
-                 influential brands.
-             </p>
-         </div>
-      </div>
+    <div className="ag-p1-container">
+        <div className='ag-p1-sec-1'>
+            <div className="ag-p1-img-div h-70 w-52 absolute top-[20%] left-[30%] rounded-[1rem] overflow-hidden">
+               <img className='h-full w-full object-cover' src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
+            </div>
+            <div className="ag-p1-text relative">
+                <div className=" mt-[50vh]">
+                    <h1 className='text-[19.5vw] uppercase leading-[16.5vw] text-center font-[700]'>Soixan7t <br/>Twelve
+                    </h1>
+                </div>
+                <div className='mt-[2rem]'>
+                   <p className='text-[3.1rem] font-[500] w-[60%] ml-[40%] leading-[10vh]'>
+                      &emsp;&emsp;&emsp;&emsp;&emsp; Our curiosity fuels our creativity. 
+                      We remain humble and say no to big egos, 
+                      even yours. A brand is alive. She has values,
+                      a personality, a history. If we forget that, we 
+                      can make good numbers in the short term, but we 
+                      kill it in the long term. This is why we are 
+                      committed to giving perspective, to building 
+                      influential brands.
+                   </p>
+                </div>
+            </div>
+        </div>
+
+        <div className='ag-p1-sec-2'>
+          
+        </div>
     </div>
   )
 }

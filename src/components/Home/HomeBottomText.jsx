ae3d84237fec489a43a34eb2fbadce60bf55c4a7
yashG <yrg.creation.ai@gmail.com>
2025-09-09 20:07:21 +0530

    working on projects page

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -4,10 +4,10 @@ import { Link, Routes, Route } from 'react-router-dom'
 const HomeBottomText = () => {
   return (
     <div className='flex items-center justify-center gap-3 text-white mt-[220px]'>
-      <div className='border-4 border-white rounded-full uppercase flex items-center '>
+      <div className='border-4 border-white rounded-full uppercase flex items-center hover:border-[#D3FD50] hover:text-[#D3FD50] '>
           <Link className= " home-b-text text-[5.6vw] px-5 leading-[6vw] " to='/Projects' >Projects</Link>
       </div>
-      <div className='border-4 border-white rounded-full uppercase flex items-center '>
+      <div className='border-4 border-white rounded-full uppercase flex items-center hover:border-[#D3FD50] hover:text-[#D3FD50] '>
           <Link className= " home-b-text text-[5.6vw] px-5 leading-[6vw] " to='/Agency' >Agency</Link>
       </div>  
     </div>

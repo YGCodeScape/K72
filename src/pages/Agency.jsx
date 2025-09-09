985155b6ac8b24192f94bc3d131ddd7b320a70dc
yashG <yrg.creation.ai@gmail.com>
2025-09-09 21:08:02 +0530

    working on agency page

--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Agency = () => {
   return (
-    <div>from Agency hello..</div>
+    <div className=''>
+       <div className=" mt-[55vh]">
+           <h1 className='text-[19.5vw] uppercase leading-[16.5vw] text-center'>Soixan7t <br/>Twelve
+           </h1>
+       </div>
+       <div className='mt-[2rem]'>
+        <p className='text-[3.1rem] font-[500] w-[60%] ml-[42%] leading-[10vh]'>
+          &emsp;&emsp;&emsp;&emsp;&emsp; Our curiosity fuels our creativity. 
+          We remain humble and say no to big egos, 
+          even yours. A brand is alive. She has values,
+           a personality, a history. If we forget that, we 
+           can make good numbers in the short term, but we 
+           kill it in the long term. This is why we are 
+           committed to giving perspective, to building 
+           influential brands.
+        </p>
+       </div>
+    </div>
   )
 }
 

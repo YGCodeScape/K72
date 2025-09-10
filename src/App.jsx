509e69414d85062c8373f5b087e9b7953b2686c5
yashG <yrg.creation.ai@gmail.com>
2025-09-10 21:55:11 +0530

    working on loading animatin

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,31 @@ import { Link, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Agency from './pages/Agency'
 import Projects from './pages/Projects'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const App = () => {
+
+  useGSAP(function(){
+    gsap.from('.stairs', {
+      height: 0,
+      duration: 2,
+      stagger: 1
+    })
+  })
+
+
   return (
     <div>
-      {/* <Link to='/'>Home</Link>
-      <Link to='/agence'>Agence</Link>
-      <Link to='/projects'>Projects</Link> */}
-      
+
+      <div className="h-screen w-full flex fixed z-10 top-0">
+        <div className="stairs h-full w-1/5 bg-black"></div>
+        <div className="stairs h-full w-1/5 bg-white"></div>
+        <div className="stairs h-full w-1/5 bg-black"></div>
+        <div className="stairs h-full w-1/5 bg-white"></div>
+        <div className="stairs h-full w-1/5 bg-black"></div>
+      </div>
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/agency' element={<Agency />} />

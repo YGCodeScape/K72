5ee3d0c4c14e317fc447c316823827e4cc12bb0a
yashG <yrg.creation.ai@gmail.com>
2025-09-10 21:59:38 +0530

    gsap animation for loading

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,13 @@ import gsap from 'gsap'
 const App = () => {
 
   useGSAP(function(){
-    gsap.from('.stairs', {
+    const tl = gsap.timeline()
+
+    tl.from('.stairs', {
       height: 0,
-      duration: 2,
-      stagger: 1
+      stagger: {
+        amount: -0.3
+      }
     })
   })
 
@@ -20,11 +23,11 @@ const App = () => {
   return (
     <div>
 
-      <div className="h-screen w-full flex fixed z-10 top-0">
+      <div className="loading-Ani h-screen w-full flex fixed z-10 top-0">
         <div className="stairs h-full w-1/5 bg-black"></div>
-        <div className="stairs h-full w-1/5 bg-white"></div>
         <div className="stairs h-full w-1/5 bg-black"></div>
-        <div className="stairs h-full w-1/5 bg-white"></div>
+        <div className="stairs h-full w-1/5 bg-black"></div>
+        <div className="stairs h-full w-1/5 bg-black"></div>
         <div className="stairs h-full w-1/5 bg-black"></div>
       </div>
 

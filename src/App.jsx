f46d8303ad0eb104e326c7d505e63f94ec589b25
yashG <yrg.creation.ai@gmail.com>
2025-09-11 22:10:07 +0530

    logo done

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { Link, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Agency from './pages/Agency'
 import Projects from './pages/Projects'
+import Navbar from './components/Navigation/Navbar'
 
 const App = () => {
 
   return (
     <div>
-
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/agency' element={<Agency />} />

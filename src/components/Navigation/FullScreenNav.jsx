34ea5f3032839da434029cb6461a025d74f2fcf6
yashG <yrg.creation.ai@gmail.com>
2025-09-12 20:49:35 +0530

    full navigation lings aded

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -2,10 +2,61 @@ import React from 'react'
 
 const FullScreenNav = () => {
   return (
-    <div className="full-nav h-screen w-full absolute py-30 bg-amber-100">
+    <div className="full-nav h-screen w-full absolute py-30 bg-black">
         <div className="fn-tags">
-            <div className="Fn-tag border-2">
-                <h1 className='text-[5rem] uppercase font-[800]'>Projects</h1>
+            <div className="Fn-tag cursor-pointer border-t-1 border-gray-300">
+                <h1 className='text-[6.5rem] uppercase font-[600] text-white leading-26 text-center'>work</h1>
+                 {/* <div>
+                     <div className="tag-slider">
+                         <h2>See everything</h2>
+                         <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt="" />
+                         <h2>See everything</h2>
+                         <img src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt="" />
+                     </div>
+                     <div className="tag-slider">
+                         <h2>See everything</h2>
+                         <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt="" />
+                         <h2>See everything</h2>
+                         <img src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt="" />
+                    </div>
+                 </div> */}
+            </div>
+            <div className="Fn-tag cursor-pointer border-t-1 border-gray-300">
+                <h1 className='text-[6.5rem] uppercase font-[600] text-white leading-26 text-center'>Agency</h1>
+                 {/* <div>
+                     <div className="tag-slider">
+                         <h2>See everything</h2>
+                         <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt="" />
+                         <h2>See everything</h2>
+                         <img src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt="" />
+                     </div>
+                     <div className="tag-slider">
+                         <h2>See everything</h2>
+                         <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt="" />
+                         <h2>See everything</h2>
+                         <img src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt="" />
+                    </div>
+                 </div> */}
+            </div>
+            <div className="Fn-tag cursor-pointer border-t-1 border-gray-300">
+                <h1 className='text-[6.5rem] uppercase font-[600] text-white leading-26 text-center'>contact</h1>
+                 {/* <div>
+                     <div className="tag-slider">
+                         <h2>See everything</h2>
+                         <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt="" />
+                         <h2>See everything</h2>
+                         <img src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt="" />
+                     </div>
+                     <div className="tag-slider">
+                         <h2>See everything</h2>
+                         <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt="" />
+                         <h2>See everything</h2>
+                         <img src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt="" />
+                    </div>
+                 </div> */}
+            </div>
+            <div className="Fn-tag cursor-pointer border-y-1 border-gray-300">
+                <h1 className='text-[6.5rem] uppercase font-[600] text-white leading-26 text-center'>blog</h1>
                  {/* <div>
                      <div className="tag-slider">
                          <h2>See everything</h2>
